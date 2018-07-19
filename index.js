4514bf7243ba5539b8c371cbce629e92c6ab4e8e
AJ <ahmed1700@windowslive.com>
2018-07-19 23:01:33 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html')
+});
+
+const port = process.env.PORT || 4040;
+app.listen(port, () => console.log(`Listening on ${port}`));
